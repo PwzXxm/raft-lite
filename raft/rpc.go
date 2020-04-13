3b45b9b0ae91c3ceab3181663df4886c5dafecf2
Zed(Zijun) Chen <c.zijunchen@gmail.com>
2020-04-13 16:36:39 +0800

    Implement RpcCall.

--- raft/rpc.go
+++ raft/rpc.go
@@ -1,9 +1,86 @@
 package raft
 
-func (p *Peer) requestVote(term, candidateId, lastLogIndex, lastLogTerm int) (int, bool) {
-	return 0, false
+import (
+	"bytes"
+	"encoding/gob"
+	"log"
+
+	"github.com/PwzXxm/raft-lite/rpccore"
+	"github.com/pkg/errors"
+)
+
+const (
+	rpcMethodRequestVote   = "rv"
+	rpcMethodAppendEntries = "ae"
+)
+
+type appendEntriesReq struct {
+	Term         int
+	LeaderId     int
+	PrevLogTerm  int
+	LeaderCommit int
+	Entries      []LogEntry
 }
 
-func (p *Peer) appendEntries(term, leaderId, prevLogIndex, prevLogTerm, leaderCommit int, entries []LogEntry) (int, bool) {
-	return 0, false
+type appendEntriesRes struct {
+	Term    int
+	Success bool
+}
+
+type requestVoteReq struct {
+	Term         int
+	CandidateId  int
+	LastLogIndex int
+	LastLogTerm  int
+}
+
+type requestVoteRes struct {
+	Term        int
+	VoteGranted bool
+}
+
+func (p *Peer) requestVote(target rpccore.NodeID, arg requestVoteReq) *requestVoteRes {
+	var res requestVoteRes
+	if p.callRpcAndLogError(target, rpcMethodRequestVote, arg, &res) {
+		return &res
+	} else {
+		return nil
+	}
+}
+
+func (p *Peer) appendEntries(target rpccore.NodeID, arg appendEntriesReq) *appendEntriesRes {
+	var res appendEntriesRes
+	if p.callRpcAndLogError(target, rpcMethodAppendEntries, arg, &res) {
+		return &res
+	} else {
+		return nil
+	}
+}
+
+func (p *Peer) callRpcAndLogError(target rpccore.NodeID, method string, req, res interface{}) bool {
+	err := p.callRpc(target, method, req, res)
+	if err != nil {
+		// TODO: we need better logging to group messages by peers.
+		log.Printf("RPC call failed. \n target: %v, method: %v, err: %+v",
+			target, method, err)
+		return false
+	}
+	return true
+}
+
+func (p *Peer) callRpc(target rpccore.NodeID, method string, req, res interface{}) error {
+	var buf bytes.Buffer
+	err := gob.NewEncoder(&buf).Encode(req)
+	if err != nil {
+		return errors.WithStack(err)
+	}
+	resData, err := p.node.SendRawRequest(target, method, buf.Bytes())
+	if err != nil {
+		// already wrapped
+		return err
+	}
+	err = gob.NewDecoder(bytes.NewReader(resData)).Decode(res)
+	if err != nil {
+		return errors.WithStack(err)
+	}
 }
