8058cdf11022b6cdf0b619e0d6ec293501d56bf1
Mars Xue <marsxue0221@gmail.com>
2020-04-18 19:31:50 +1000

    Add random function

--- utils/utils.go
+++ utils/utils.go
@@ -1,8 +1,19 @@
 package utils
 
+import (
+	"math/rand"
+	"time"
+)
+
 func Min(a, b int) int {
 	if a < b {
 		return a
 	}
 	return b
+}
+
+// Random an integer within the range
+func Random(a, b int) int {
+	rand.Seed(time.Now().UnixNano())
+	return rand.Intn(b-a+1) + a
 }
