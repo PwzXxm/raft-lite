9f52f54623dd3d90fe1b27d93443033ba7627b95
Minjian Chen <cmjhaha886@gmail.com>
2020-04-22 20:34:08 +0800

    _

--- raft/append_entry.go
+++ raft/append_entry.go
@@ -38,29 +38,76 @@ func (p *Peer) consitencyCheck(req appendEntriesReq) bool {
 	return true
 }
 
-//iteratively call appendEntry RPC until getting success result
-func (p *Peer) callAppendEntryRPC(target rpccore.NodeID) {
+//iteratively call appendEntry RPC until getting success result or no longer being leader
+func (p *Peer) callAppendEntryRPC(target rpccore.NodeID) bool {
+	appendSuccess := false
+	p.mutex.Lock()
 	nextIndex := p.nextIndex[target]
 	prevLogTerm := p.log[nextIndex-1].term
+	p.mutex.Unlock()
 	// call append entry RPC
 	req := appendEntriesReq{Term: p.currentTerm, LeaderID: p.node.NodeID(), PrevLogIndex: nextIndex - 1, PrevLogTerm: prevLogTerm, LeaderCommit: p.commitIndex, Entries: p.log[nextIndex:]}
 	res := p.appendEntries(target, req)
-	for res.Success == false && p.state == Leader {
-		// update nextIndex for target node
-		p.mutex.Lock()
-		p.nextIndex[target]--
-		p.mutex.Unlock()
-		// update and resend appendEntry request
-		nextIndex = p.nextIndex[target]
-		req.PrevLogIndex = nextIndex - 1
-		req.PrevLogTerm = p.log[nextIndex-1].term
-		req.Entries = p.log[nextIndex:]
-		res = p.appendEntries(target, req)
+	for true {
+		if res == nil {
+			// retry call appendEntries rpc
+			res = p.appendEntries(target, req)
+		} else if res.Success == false {
+			// update nextIndex for target node
+			p.mutex.Lock()
+			p.nextIndex[target]--
+			// update and resend appendEntry request
+			nextIndex = p.nextIndex[target]
+			req.PrevLogIndex = nextIndex - 1
+			req.PrevLogTerm = p.log[nextIndex-1].term
+			req.Entries = p.log[nextIndex:]
+			p.mutex.Unlock()
+			res = p.appendEntries(target, req)
+		} else {
+			//if success, update nextIndex for the node
+			p.mutex.Lock()
+			p.nextIndex[target] = p.nextIndex[target] + len(req.Entries)
+			p.mutex.Unlock()
+			appendSuccess = true
+			break
+		}
+		// TODO: add other conditions that should stop sending request
+		if p.state != Leader {
+			break
+		}
 	}
-	//if success, update nextIndex for the node
-	if res.Success == true && p.state == Leader {
-		p.mutex.Lock()
-		p.nextIndex[target] = p.nextIndex[target] + len(req.Entries)
-		p.mutex.Unlock()
+	return appendSuccess
+}
+
+func (p *Peer) onReceiveClientRequest(newlog LogEntry) bool {
+	majorityCheckChannel := make(chan bool)
+	p.mutex.Lock()
+	p.log = append(p.log, newlog)
+	peersIDs := p.rpcPeersIds
+	totalPeers := len(p.rpcPeersIds)
+	p.mutex.Unlock()
+	for _, rpcPeerID := range peersIDs {
+		go func() {
+			success := p.callAppendEntryRPC(rpcPeerID)
+			majorityCheckChannel <- success
+		}()
 	}
+	// check channel, while true >= half of peers, return true, when len = len(peers), return false
+	var result bool
+	var totalCount int
+	var agreeCount int
+	for true {
+		result = <-majorityCheckChannel
+		if result {
+			agreeCount++
+		}
+		totalCount++
+		if 2*agreeCount >= totalPeers {
+			return true
+		}
+		if totalCount >= totalPeers {
+			return false
+		}
+	}
+	return false
 }
