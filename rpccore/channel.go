e2656510e308737ae6755095e0cbc4c0b11ff129
Zed(Zijun) Chen <c.zijunchen@gmail.com>
2020-04-30 15:35:36 +0800

    Implement tcp rpccore.

--- rpccore/channel.go
+++ rpccore/channel.go
@@ -13,6 +13,7 @@ type ChanNode struct {
 	id       NodeID
 	network  *ChanNetwork
 	callback Callback
+	lock     sync.RWMutex
 }
 
 func (node *ChanNode) NodeID() NodeID {
@@ -25,8 +26,8 @@ func (node *ChanNode) SendRawRequest(target NodeID, method string, data []byte) 
 	deadline := time.Now().Add(node.network.timeout)
 	node.network.lock.RUnlock()
 	if ok {
-		resChan := make(chan *resMsg)
-		req := reqMsg{source: node.id, method: method, data: data,
+		resChan := make(chan *chanResMsg)
+		req := chanReqMsg{source: node.id, method: method, data: data,
 			resChan: resChan, deadline: deadline}
 		reqChan <- &req
 		remainingTime := deadline.Sub(time.Now())
@@ -47,12 +48,14 @@ func (node *ChanNode) SendRawRequest(target NodeID, method string, data []byte) 
 }
 
 func (node *ChanNode) RegisterRawRequestCallback(callback Callback) {
+	node.lock.Lock()
 	node.callback = callback
+	node.lock.Unlock()
 }
 
 type ChanNetwork struct {
 	lock           sync.RWMutex
-	nodeChannelMap map[NodeID](chan *reqMsg)
+	nodeChannelMap map[NodeID](chan *chanReqMsg)
 	timeout        time.Duration
 	delayGenerator DelayGenerator
 }
@@ -60,7 +63,7 @@ type ChanNetwork struct {
 func NewChanNetwork(timeout time.Duration) *ChanNetwork {
 	n := new(ChanNetwork)
 	n.lock = sync.RWMutex{}
-	n.nodeChannelMap = make(map[NodeID](chan *reqMsg))
+	n.nodeChannelMap = make(map[NodeID](chan *chanReqMsg))
 	n.timeout = timeout
 	n.delayGenerator = func(source, target NodeID) time.Duration {
 		return 0
@@ -78,9 +81,13 @@ func (n *ChanNetwork) NewNode(nodeID NodeID) (*ChanNode, error) {
 	}
 	node := new(ChanNode)
 	node.network = n
+	node.lock = sync.RWMutex{}
 	node.id = nodeID
+	node.callback = func(source NodeID, method string, data []byte) ([]byte, error) {
+		return nil, errors.New("No callback function provided.")
+	}
 	// don't need buffer here since the send is blocking anyway.
-	nodeChannel := make(chan *reqMsg)
+	nodeChannel := make(chan *chanReqMsg)
 	n.nodeChannelMap[node.id] = nodeChannel
 
 	// start listening loop
@@ -89,8 +96,11 @@ func (n *ChanNetwork) NewNode(nodeID NodeID) (*ChanNode, error) {
 			if node.callback == nil {
 				log.Fatalf("Cannot find Callback for NodeID: %v.", node.id)
 			}
+			node.lock.RLock()
+			callback := node.callback
+			node.lock.RUnlock()
 			// start a new goroutine for handling this request
-			go func(req *reqMsg) {
+			go func(req *chanReqMsg) {
 				// delay (sender to receiver)
 				n.lock.RLock()
 				delay := n.delayGenerator(req.source, nodeID)
@@ -98,18 +108,18 @@ func (n *ChanNetwork) NewNode(nodeID NodeID) (*ChanNode, error) {
 				time.Sleep(delay)
 				remainingTime := req.deadline.Sub(time.Now())
 				if remainingTime <= 0 {
-					req.resChan <- &resMsg{data: nil,
+					req.resChan <- &chanResMsg{data: nil,
 						err: errors.New("Request timeout.")}
 				} else {
 					// invoke callback
-					data, err := node.callback(req.source, req.method, req.data)
+					data, err := callback(req.source, req.method, req.data)
 					// delay (receiver to sender)
 					n.lock.RLock()
 					delay := n.delayGenerator(nodeID, req.source)
 					n.lock.RUnlock()
 					if delay <= remainingTime {
 						time.Sleep(delay)
-						req.resChan <- &resMsg{data: data, err: err}
+						req.resChan <- &chanResMsg{data: data, err: err}
 					}
 				}
 
@@ -128,16 +138,16 @@ func (n *ChanNetwork) SetDelayGenerator(delayGenerator DelayGenerator) {
 }
 
 // we are passing pointer in the channel, we should treat those two
-// structs as immutable
-type reqMsg struct {
+// those structs should be immutable
+type chanReqMsg struct {
 	source   NodeID
 	method   string
 	data     []byte
-	resChan  chan *resMsg
+	resChan  chan *chanResMsg
 	deadline time.Time
 }
 
-type resMsg struct {
+type chanResMsg struct {
 	err  error
 	data []byte
 }
