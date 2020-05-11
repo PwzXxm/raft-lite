2da310756bbe373b016419bb224a3864b5c4f05b
Mars Xue <marsxue0221@gmail.com>
2020-05-11 23:25:58 +1000

    Update

--- rpccore/rpccore_test.go
+++ rpccore/rpccore_test.go
@@ -22,37 +22,8 @@ func TestNewNode(t *testing.T) {
 	}
 
 	_, err = network.NewNode(NodeID("node"))
-
 	if err == nil {
 		t.Errorf("Node B should have same ID with A")
-	}
-}
-
-func TestChannelCommunication(t *testing.T) {
-	network := NewChanNetwork(time.Second)
-
-	nodeA, _ := network.NewNode("nodeA")
-	nodeB, _ := network.NewNode("nodeB")
-	nodeC, _ := network.NewNode("nodeC")
-
-	nodeB.RegisterRawRequestCallback(func(source NodeID, method string, data []byte) ([]byte, error) {
-		if string(data) == "Test: A -> B" {
-			return []byte(string(source)), nil
-		} else {
-			return []byte(string(source)), errors.New("Incorrect data")
-		}
-	})
-
-	data := []byte("Test: A -> B")
-	_, err := nodeA.SendRawRequest(nodeB.NodeID(), "test", data)
-	if err != nil {
-		t.Errorf("Node A should receive callback.\n%+v", err)
-	}
-
-	data = []byte("Test: C -> B")
-	_, err = nodeC.SendRawRequest(nodeB.NodeID(), "test", data)
-	if err == nil {
-		t.Errorf("Node C should receive error")
 	}
 }
 
@@ -89,7 +60,7 @@ func TestTimeoutAndDelayGenerator(t *testing.T) {
 }
 
 func TestNewTCPNetwork(t *testing.T) {
-	tcpNetwork := NewTCPNetwork(1 * time.Second)
+	tcpNetwork := NewTCPNetwork(time.Second)
 
 	if tcpNetwork.timeout != time.Second {
 		t.Errorf("TCPNetwork timeout should have the same value.")
@@ -97,9 +68,9 @@ func TestNewTCPNetwork(t *testing.T) {
 }
 
 func TestNewRemoteNode(t *testing.T) {
-	tcpNetwork := NewTCPNetwork(1 * time.Second)
-	remoteNode := tcpNetwork.NewRemoteNode("node", "addr")
+	tcpNetwork := NewTCPNetwork(time.Second)
 
+	remoteNode := tcpNetwork.NewRemoteNode("node", "addr")
 	if remoteNode != nil {
 		t.Errorf("RemoteNode should return nil error value.")
 	}
@@ -111,9 +82,9 @@ func TestNewRemoteNode(t *testing.T) {
 }
 
 func TestNewLocalNode(t *testing.T) {
-	tcpNetwork := NewTCPNetwork(1 * time.Second)
+	tcpNetwork := NewTCPNetwork(time.Second)
 
-	localNode, err := tcpNetwork.NewLocalNode("nodeB", "127.0.0.1:1112", ":1112")
+	localNode, err := tcpNetwork.NewLocalNode("nodeB", "127.0.0.1:1110", ":1110")
 	if err != nil {
 		t.Errorf("LocalNode should return nil error value.")
 	}
@@ -122,20 +93,49 @@ func TestNewLocalNode(t *testing.T) {
 	}
 
 	// check node with same ID already exists
-	localNode_, err_ := tcpNetwork.NewLocalNode("nodeB", "127.0.0.1:1112", ":1112")
+	localNode_, err_ := tcpNetwork.NewLocalNode("nodeB", "127.0.0.1:1110", ":1110")
 	if localNode_ != nil && err_ == nil {
 		t.Errorf("LocalNode should return error value.")
 	}
 }
 
-func TestTCPCommunication(t *testing.T) {
-	tcpNetwork := NewTCPNetwork(1 * time.Second)
-	nodeA, _ := tcpNetwork.NewLocalNode("nodeA", "127.0.0.1:1111", ":1111")
-	_ = tcpNetwork.NewRemoteNode("nodeB", "127.0.0.1:2222")
+func checkNoError(t *testing.T, err error) {
+	if err != nil {
+		t.Errorf("Shouldn't be an error: %+v", errors.WithStack(err))
+	}
+}
 
-	tcpNetwork_ := NewTCPNetwork(1 * time.Second)
-	nodeB, _ := tcpNetwork_.NewLocalNode("nodeB", "127.0.0.1:2222", ":2222")
+func TestChanNode(t *testing.T) {
+	network := NewChanNetwork(time.Second)
+	nodeA, err := network.NewNode("nodeA")
+	checkNoError(t, err)
+	nodeB, err := network.NewNode("nodeB")
+	checkNoError(t, err)
 
+	testNode(t, nodeA, nodeB)
+}
+
+func TestTCPNode(t *testing.T) {
+	networkA := NewTCPNetwork(time.Second)
+	nodeA, err := networkA.NewLocalNode("nodeA", "127.0.0.1:1111", ":1111")
+	checkNoError(t, err)
+	err = networkA.NewRemoteNode("nodeB", "127.0.0.1:1112")
+	checkNoError(t, err)
+
+	networkB := NewTCPNetwork(time.Second)
+	nodeB, err := networkB.NewLocalNode("nodeB", "127.0.0.1:1112", ":1112")
+	checkNoError(t, err)
+
+	testNode(t, nodeA, nodeB)
+}
+
+func testNode(t *testing.T, nodeA Node, nodeB Node) {
+	// test NodeID
+	if nodeA.NodeID() != "nodeA" || nodeB.NodeID() != "nodeB" {
+		t.Errorf("Node has incorrect NodeID.")
+	}
+
+	// test RegisterRawRequestCallback
 	callback := func(source NodeID, method string, data []byte) ([]byte, error) {
 		if string(data) == "Test: A -> B" {
 			return []byte(string(source)), nil
@@ -143,44 +143,51 @@ func TestTCPCommunication(t *testing.T) {
 			return []byte(string(source)), errors.New("Incorrent data")
 		}
 	}
-
 	nodeB.RegisterRawRequestCallback(callback)
 
+	// test SendRawRequest
 	data := []byte("Test: A -> B")
-	_, err := nodeA.SendRawRequest("nodeB", "test", data)
+	_, err := nodeA.SendRawRequest(nodeB.NodeID(), "test", data)
 	if err != nil {
-		t.Errorf("Node A should receive a response.\n%+v", err)
+		t.Errorf("Node A should receive callback.\n%+v", err)
 	}
 
 	data = []byte("Test: B -> A")
-
-	// node with same id already exists
-	_, err = tcpNetwork.NewLocalNode("nodeA", "127.0.0.1:1111", ":1111")
+	_, err = nodeB.SendRawRequest(nodeA.NodeID(), "test", data)
 	if err == nil {
-		t.Errorf("Node B should receive duplicate node errer.")
-	}
-
-	// unable to find target node
-	_, err = nodeB.SendRawRequest("nodeA", "test", data)
-	if err == nil {
-		t.Errorf("Node B should receive no target node errer.")
-	}
-
-	// no callback function provided
-	_ = tcpNetwork_.NewRemoteNode("nodeA", "127.0.0.1:1111")
-	_, err = nodeB.SendRawRequest("nodeA", "test", data)
-	if err == nil {
-		t.Errorf("Node B should receive no callback errer.")
+		t.Errorf("Node B should receive no callback error.")
 	}
 }
 
-func BenchmarkCommunication(b *testing.B) {
+func BenchmarkChanCommunication(b *testing.B) {
 	network := NewChanNetwork(time.Second)
-
 	nodeA, _ := network.NewNode("nodeA")
 	nodeB, _ := network.NewNode("nodeB")
 	nodeC, _ := network.NewNode("nodeC")
 
+	benchmarkCommunication(b, nodeA, nodeB, nodeC)
+}
+
+func BenchmarkTCPCommunication(b *testing.B) {
+	networkA := NewTCPNetwork(time.Second)
+	nodeA, _ := networkA.NewLocalNode("nodeA", "127.0.0.1:1113", ":1113")
+	_ = networkA.NewRemoteNode("nodeB", "127.0.0.1:1114")
+	_ = networkA.NewRemoteNode("nodeC", "127.0.0.1:1115")
+
+	networkB := NewTCPNetwork(time.Second)
+	nodeB, _ := networkB.NewLocalNode("nodeB", "127.0.0.1:1114", ":1114")
+	_ = networkB.NewRemoteNode("nodeA", "127.0.0.1:1113")
+	_ = networkB.NewRemoteNode("nodeC", "127.0.0.1:1115")
+
+	networkC := NewTCPNetwork(time.Second)
+	nodeC, _ := networkC.NewLocalNode("nodeC", "127.0.0.1:1115", ":1115")
+	_ = networkC.NewRemoteNode("nodeA", "127.0.0.1:1113")
+	_ = networkC.NewRemoteNode("nodeB", "127.0.0.1:1114")
+
+	benchmarkCommunication(b, nodeA, nodeB, nodeC)
+}
+
+func benchmarkCommunication(b *testing.B, nodeA Node, nodeB Node, nodeC Node) {
 	callbackHandler := func(source NodeID, method string, data []byte) ([]byte, error) {
 		return []byte(string(source)), nil
 	}
