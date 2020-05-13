ff742bcce9fda7b614b19890ed3d03e61e2410d9
Minjian Chen <cmjhaha886@gmail.com>
2020-05-13 21:21:11 +0800

    _

--- clicmd/config.go
+++ clicmd/config.go
@@ -11,36 +11,44 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-type tcpConfig struct {
-	timeout          time.Duration
-	nodeAddrMap      map[rpccore.NodeID]string
-	nodeID           rpccore.NodeID
-	remoteAddr       string
-	listenAddr       string
-	peers            []rpccore.NodeID
-	sm               sm.StateMachine
-	pstorageFilePath string
-	logPath          string
+type raftConfig struct {
+	Timeout          time.Duration
+	NodeAddrMap      map[rpccore.NodeID]string
+	NodeID           rpccore.NodeID
+	RemoteAddr       string
+	ListenAddr       string
+	Peers            []rpccore.NodeID
+	Sm               sm.StateMachine
+	PstorageFilePath string
+	LogPath          string
 }
 
 //StartFromFile is good
 func StartFromFile(filepath string) error {
-	tcpConfig := readFromJSON(filepath)
-	n := rpccore.NewTCPNetwork(tcpConfig.timeout)
-	node, err := n.NewLocalNode(tcpConfig.nodeID, tcpConfig.remoteAddr, tcpConfig.listenAddr)
+	config := readFromJSON(filepath)
+	n := rpccore.NewTCPNetwork(config.Timeout)
+	node, err := n.NewLocalNode(config.NodeID, config.RemoteAddr, config.ListenAddr)
 	if err != nil {
 		return err
 	}
-	for nodeID, addr := range tcpConfig.nodeAddrMap {
+	for nodeID, addr := range config.NodeAddrMap {
 		n.NewRemoteNode(nodeID, addr)
 	}
 	logger := logrus.New()
-	logger.Out = os.Stdout
-	ps := pstorage.NewFileBasedPersistentStorage(tcpConfig.pstorageFilePath)
-	p, err := raft.NewPeer(node, tcpConfig.peers, logger, tcpConfig.sm, ps)
+
+	file, err := os.OpenFile(config.LogPath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err == nil {
+		logger.Out = file
+	} else {
+		logger.Info("Failed to log to file, using default stderr")
+		logger.Out = os.Stdout
+	}
+	ps := pstorage.NewFileBasedPersistentStorage(config.PstorageFilePath)
+	p, err := raft.NewPeer(node, config.Peers, logger.WithFields(logrus.Fields{
+		"nodeID": node.NodeID(),}), config.Sm, ps)
 	return nil
 }
 
-func readFromJSON(filepath string) tcpConfig {
-	return tcpConfig{}
+func readFromJSON(filepath string) raftConfig {
+	return raftConfig{}
 }
