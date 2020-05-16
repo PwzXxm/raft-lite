2ea3f6de9465a0b6de7f0944329590d93e5c98b3
Zed(Zijun) Chen <c.zijunchen@gmail.com>
2020-05-16 22:44:36 +0800

    _

--- client/actions.go
+++ client/actions.go
@@ -1,12 +1,14 @@
 package client
 
 import (
+	"fmt"
 	"os"
 	"time"
 
 	"github.com/PwzXxm/raft-lite/rpccore"
 	"github.com/PwzXxm/raft-lite/sm"
 	"github.com/PwzXxm/raft-lite/utils"
+	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
 )
 
@@ -22,26 +24,28 @@ type Client struct {
 	ab       *sm.TSMActionBuilder
 
 	logger logrus.Logger
+
+	leaderID *rpccore.NodeID
 }
 
 func NewClientFromConfig(config interface{}) *Client {
 	c := new(Client)
 
-    // TODO: init
-    c.n = 
-    c.clientID = 
-    c.net = rpccore.NewTCPNetwork(tcpTimeout)
+	// TODO: init
+	// c.n =
+	// c.clientID =
+	c.net = rpccore.NewTCPNetwork(tcpTimeout)
 
-    // TODO: change to no server version
-    c.node = c.net.NewLocalNode()
+	// TODO: change to no server version
+	// c.node = c.net.NewLocalNode()
 
-    c.nl = 
-    c.peers = 
+	// c.nl =
+	// c.peers =
 
-    c.ab = sm.NewTSMActionBuilder(c.clientID)
+	c.ab = sm.NewTSMActionBuilder(c.clientID)
 
-    c.looger = logrus.New()
-    c.logger.Out = os.Stdout
+	// c.looger = logrus.New()
+	c.logger.Out = os.Stdout
 
 	return c
 }
@@ -49,40 +53,98 @@ func NewClientFromConfig(config interface{}) *Client {
 func (c *Client) parseAndBuild(cmd interface{}) (interface{}, error) {
 	// parse
 
-	c.ab.TSMActionIncrValue()
-	c.ab.TSMActionMoveValue()
-	c.ab.TSMActionSetValue()
-	sm.NewTSMDataQuery(d)
+	// c.ab.TSMActionIncrValue()
+	// c.ab.TSMActionMoveValue()
+	// c.ab.TSMActionSetValue()
+	// sm.NewTSMDataQuery(d)
+	return nil, nil
 }
 
-func (c *Client) Request(cmd interface{}) error {
-	tsmAction, err := parseAndBuild(cmd)
-
-	if err != nil {
-		return err
-	}
-
-	for {
-		// find leader
+func (c *Client) lookForLeader() rpccore.NodeID {
+	// cached, the cache will be cleaned if there is any issue
+	// blocking, keep trying until find a leader
+	for c.leaderID != nil {
 		pl := c.peers[c.nl[utils.Random(0, c.n-1)]].NodeID()
-
 		var leaderRes LeaderRes
 		err := c.callRPC(pl, RPCMethodLeaderRequest, "", &leaderRes)
+		if err == nil {
+			if leaderRes.HasLeader {
+				c.logger.Infof("Node %v answered with leader = %v", pl,
+					leaderRes.LeaderID)
+				c.leaderID = &leaderRes.LeaderID
+				return *c.leaderID
+			} else {
+				err = errors.Errorf("Node %v doesn't know the leader.", pl)
+			}
+		}
+		c.logErrAndBackoff("Unable to find leader. ", err)
+	}
+	return *c.leaderID
+}
+
+func (c *Client) logErrAndBackoff(msg string, err error) {
+	c.leaderID = nil
+	c.logger.Debug(msg, err)
+	// TODO: better backoff strategy?
+	time.Sleep(100 * time.Millisecond)
+}
+
+func (c *Client) sendActionRequest(act sm.TSMAction) error {
+	leader := c.lookForLeader()
+	fmt.Print(leader)
+	return nil
+}
+
+func (c *Client) checkActionRequest(act sm.TSMAction) (bool, error) {
+	leader := c.lookForLeader()
+	fmt.Print(leader)
+	return false, nil
+}
+
+func (c *Client) executeActionRequest(act sm.TSMAction) {
+	for {
+		err := c.sendActionRequest(act)
 		if err != nil {
-			c.logger.Warn("RPC call failed ", err)
+			c.logErrAndBackoff("send action request failed. ", err)
 			continue
 		}
 
-		if leaderRes.HasLeader {
-            l := leaderRes.LeaderID
-            
+		// TODO: avg success time?
+		time.Sleep(100 * time.Millisecond)
 
-
-
-			c.callRPC(l)
-
-			sm.NewTSMLatestRequestQuery()
+		for i := 0; i < 4; i++ {
+			success, err := c.checkActionRequest(act)
+			if err != nil {
+				c.logErrAndBackoff("check action request failed. ", err)
+			}
+			if success {
+				c.logger.Infof("action success.")
+				return
+			} else {
+				// TODO: another backoff?
+				time.Sleep(100 * time.Millisecond)
+			}
 		}
 	}
+}
+
+func (c *Client) Request(cmd interface{}) error {
+	// tsmAction, err := parseAndBuild(cmd)
+
+	// if err != nil {
+	// 	return err
+	// }
+
+	// for {
+
+	// 	if leaderRes.HasLeader {
+	// 		l := leaderRes.LeaderID
+
+	// 		c.callRPC(l)
+
+	// 		sm.NewTSMLatestRequestQuery()
+	// 	}
+	// }
+	// return nil
 	return nil
 }
