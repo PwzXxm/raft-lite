963249a674a61c22d7684b0d302e8b20ccd371f6
Shijie Liu <shijiel2@student.unimelb.edu.au>
2020-05-19 15:04:42 +1000

    delete print

--- raft/peer.go
+++ raft/peer.go
@@ -1,7 +1,6 @@
 package raft
 
 import (
-	"fmt"
 	"sync"
 	"time"
 
@@ -284,7 +283,6 @@ func (p *Peer) updateCommitIndex(idx int) {
 	if idx > p.commitIndex {
 		for i := p.commitIndex + 1; i <= idx; i++ {
 			action := p.log[p.toLogIndex(i)].Cmd
-			fmt.Printf("action is %v\n", action)
 			if action != nil {
 				err := p.stateMachine.ApplyAction(action)
 				if err != nil {
