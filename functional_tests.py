a435f6fea5edae86cf4bee7afb4237bdc3eacba0
Shijie Liu <shijiel2@student.unimelb.edu.au>
2020-05-20 23:08:13 +1000

    update log term

--- functional_tests.py
+++ functional_tests.py
@@ -26,19 +26,24 @@ def main():
                         help='number of tests allowed to run in parallel')
     parser.add_argument('--timeout', type=int, default=5*60,
                         help='time limit for each tests (in second)')
+    parser.add_argument('--single_testcase', type=int, default=0,
+                        help='single testcase number')
     args = parser.parse_args()
     print(args)
-    if not run_tests(args.times, args.parallel, args.timeout):
+    if not run_tests(args.times, args.parallel, args.timeout, args.single_testcase):
         sys.exit(1)
+
 
 def build_executable() -> None:
     res = subprocess.run(["go", "build", "."],
-                            timeout=20, stdout=subprocess.PIPE,
-                            stderr=subprocess.STDOUT, encoding="utf-8")
+                         timeout=20, stdout=subprocess.PIPE,
+                         stderr=subprocess.STDOUT, encoding="utf-8")
     if res.returncode != 0:
         raise RuntimeError("Build Fail"+res.stdout)
 
 # Return values are (passed, timeout, output)
+
+
 def execute_raft_lite(args: List[str], timeout: int) -> Tuple[bool, bool, str]:
     try:
         res = subprocess.run(['./raft-lite']+args,
@@ -65,7 +70,7 @@ def run_single_test(task: Tuple[int, int, int]) -> Tuple[bool, bool, str]:
     return (passed, timeout, tests_list)
 
 
-def run_tests(times: int, parallel: int, timeout: int) -> bool:
+def run_tests(times: int, parallel: int, timeout: int, single_testcase: int) -> bool:
     build_executable()
     (ok, _, out) = execute_raft_lite(["functionaltest", "count"], timeout)
     if not ok:
@@ -75,7 +80,11 @@ def run_tests(times: int, parallel: int, timeout: int) -> bool:
     if not ok:
         raise RuntimeError("Unable to load test cases")
     tests_n = int(out)
-    tasks = [(i+1, j+1, timeout) for i in range(tests_n) for j in range(times)]
+    if single_testcase != 0:
+        tasks = [(single_testcase+1, j+1, timeout) for j in range(times)]
+    else:
+        tasks = [(i+1, j+1, timeout)
+                 for i in range(tests_n) for j in range(times)]
     print("="*40)
     print("Start running tests")
     print(tests_list)
@@ -91,6 +100,8 @@ def run_tests(times: int, parallel: int, timeout: int) -> bool:
     # print results
     stats = [[0, 0, 0] for _ in range(tests_n)]
     for ((test_id, _, _), (ok, timeout, _)) in zip(tasks, results):
+        if single_testcase != 0:
+            test_id = single_testcase
         if ok:
             stats[test_id-1][0] += 1
         elif timeout:
